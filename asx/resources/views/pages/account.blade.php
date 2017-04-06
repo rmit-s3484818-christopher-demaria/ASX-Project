5753582973c32d8cba86560882725b0f3b3dcd71
mattringin <mattringin@hotmail.com>
2017-04-06 20:59:54 +1000

    Basic leaderboard made

--- asx/resources/views/pages/account.blade.php
+++ asx/resources/views/pages/account.blade.php
@@ -6,6 +6,7 @@
 @section('body')
     <h1 class="headers">Account Page</h1>
     <p><strong>Welcome {{ Auth::user()->name }}!</strong></p>
+    <p><strong>Your account balance is: ${{ Auth::user()->money }}</strong></p>
     <p>This will be our account page</p>
     <p>Text added by andy as a test. Will be deleted</p>
 @endsection
--- asx/resources/views/pages/leaderboard.blade.php
+++ asx/resources/views/pages/leaderboard.blade.php
@@ -5,5 +5,23 @@
 @stop
 @section('body')
     <h1 class="headers">Leader Board Page</h1>
-    <p>This is where the leaderboard will be</p>
+
+ <?php $users = DB::table('users')->orderBy('money', 'desc')->get(); ?>
+
+    <table class="leaderboard">
+        <caption>Overall Leaders</caption>
+        <tr>
+            <th>Ranking</th>
+            <th>Name</th>
+            <th>Score(networth)</th>
+        </tr>
+        @foreach ($users as $user)
+            <tr>
+                <th> {{ $loop->iteration }} </th>
+                <th> {{ $user->name }} </th>
+                <th> ${{ number_format($user->money, 2) }} </th>
+            </tr>
+        @endforeach
+    </table>
+
 @endsection
