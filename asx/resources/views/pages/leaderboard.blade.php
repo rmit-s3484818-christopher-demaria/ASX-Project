e7713f7a8781dabb1b6df57412223d1f652d20b7
rmit-s3432729-andrew-sekulovski <s3432729@student.rmit.edu.au>
2017-04-06 21:48:18 +1000

    minor changes to font

--- asx/resources/views/pages/leaderboard.blade.php
+++ asx/resources/views/pages/leaderboard.blade.php
@@ -22,12 +22,12 @@
                         <table class="leader-table table-striped table table-responsive">
                             <tr class="leader-headings info">
                                 <td align="center" class="ranking-col">Ranking</td>
-                                <td>Name</td>
-                                <td align="right">Score(networth)</td>
+                                <td>Username</td>
+                                <td align="right">Score/Net Worth</td>
                             </tr>
                             @foreach ($users as $user)
                                 <tr>
-                                    <td align="center"> {{ $loop->iteration }} </td>
+                                    <td align="center"><strong>{{ $loop->iteration }}</strong></td>
                                     <td> {{ $user->name }} </td>
                                     <td align="right"> ${{ number_format($user->money, 2) }} </td>
                                 </tr>
