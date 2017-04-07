374a5c69c0ca4a826ce1a0d08f9e3b3479ffab2b
rmit-s3432729-andrew-sekulovski <s3432729@student.rmit.edu.au>
2017-04-07 13:58:44 +1000

    Navbar working
    Updated navbar. Working correctly

--- asx/resources/views/pages/account.blade.php
+++ asx/resources/views/pages/account.blade.php
@@ -4,10 +4,10 @@
 @stop
 @section('body')
     {{--<h1 class="headers">Account Page</h1>--}}
-    <p><strong>Welcome {{ Auth::user()->name }}!</strong></p>
+    {{--<p><strong>Welcome {{ Auth::user()->name }}!</strong></p>--}}
 
-    <p>This will be our account page</p>
-    <p>Text added by andy as a test. Will be deleted</p>
+    {{--<p>This will be our account page</p>--}}
+    {{--<p>Text added by andy as a test. Will be deleted</p>--}}
 
 
 
@@ -58,10 +58,10 @@
             </div>
         </div>
     </div>
-    =======
-    <h1 class="headers">Account Page</h1>
-    <p><strong>Welcome {{ Auth::user()->name }}!</strong></p>
-    <p>This will be our account page</p>
-    <p>Text added by andy as a test. Will be deleted</p>
-    >>>>>>> origin/master
+    {{--=======--}}
+    {{--<h1 class="headers">Account Page</h1>--}}
+    {{--<p><strong>Welcome {{ Auth::user()->name }}!</strong></p>--}}
+    {{--<p>This will be our account page</p>--}}
+    {{--<p>Text added by andy as a test. Will be deleted</p>--}}
+    {{-->>>>>>> origin/master--}}
 @endsection
--- asx/resources/views/shared/navbar.blade.php
+++ asx/resources/views/shared/navbar.blade.php
@@ -1,4 +1,49 @@
 <!-- NAV BAR - Generic sticky navbar, improvements will be made later -->
+
+
+<div class="sidebarNav">
+    <!-- Sidebar -->
+    <div class="sidebarNav">
+
+        <div class="row-fluid">
+            <div class="row-fluid playerContainer">
+                <div class="col-lg-1 col-md-1 col-lg-offset-1 col-md-offset-1 playerTile"><h3>Icon</h3></div>
+                <div class="col-lg-8 col-md-8 col-lg-offset-1 col-md-offset-1 playerTile"><h3>user10112</h3></div> <!-- ensure username is max 15 characters long -->
+            </div>
+
+            <ul class="sidebarOptions">
+                <a href="{{ url("/")}}"><li class="dashboardTile"><h4>Dashboard</h4></li></a>
+                @if (Auth::check())
+                    <a href="{{ url("account") }}"> <li class="portfolioTile"><h4>Portfolio</h4></li></a>
+                @endif
+                {{--<li class="portfolioTile"><h4>Portfolio</h4></li>--}}
+                <a href="{{ url("leader")}}"><li class="leaderboardTile"><h4>Leaderboard</h4></li></a>
+                <a href="{{ url("market")}}"><li class="marketTile"><h4>Market</h4></li></a>
+                {{--<li class="leaderboardTile"><h4>Leaderboard</h4></li>--}}
+                {{--<li class="marketTile"><h4>Market</h4></li>--}}
+                @if (Route::has('login'))
+
+                    @if (Auth::check())
+                        <li><a href="{{ url('auth/logout') }}">Logout</a></li>
+                    @else
+                        <li><a href="{{ url('/login') }}">Login</a></li>
+                        <li><a href="{{ url('/register') }}">Register</a></li>
+                    @endif
+
+                @endif
+            </ul>
+
+            <div class="row-fluid sidebarBottom">
+                <div class="col-lg-1 col-md-1 col-md-offset-1 col-lg-offset-1 sidebarBottomContainer">Settings</div>
+                <div class="col-lg-1 col-lg-offset-5 col-md-offset-5 col-md-1 sidebarBottomContainer"><button class="btn-danger btn-logout">Log Out</button></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
 <ul>
     <li><a href="{{ url("/")}}">Home</a></li>
     @if (Auth::check())
