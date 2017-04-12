3cfe4af7edb08ec868294e4a6b4f59517b0af187
rmit-s3484818-christopher-demaria <s3484818@student.rmit.edu.au>
2017-04-12 21:30:56 +1000

    Couldnt get Excel working, will attempt later
    If your laravel doesnt work, message me how to fix it

--- asx/app/Http/Controllers/createAsxList.php
+++ asx/app/Http/Controllers/createAsxList.php
@@ -5,46 +5,57 @@ namespace App\Http\Controllers;
 use App\asxList;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Collection;
 
 class createAsxList extends BaseController
 {
     public function findAsxList()
     {
-        $list = [];
+//        $list = [];
+//
+//            $handle = fopen("/asxStock/asx.csv", "r");
+//
+//            while(! feof($handle))
+//            {
+//                $stock = fgetcsv($handle);
+//
+//                $stock[3] = basename($handle, '.csv');
+//
+//                if(count($stock) == 4)
+//                {
+//                    $list[] = $stock;
+//                }
+//            }
+//
+//        fclose($handle);
+//
+//        /**  ppd($stocks); **/
+//
+//        echo count($list) . ' to create<br/>';
+//
+//        foreach($list as $stock)
+//        {
+//            $row = asxList::create(
+//                [
+//                    'name' => $stock[0],
+//                    'symbol' => $stock[1],
+//                    'updated_at' => '5'
+//                ]
+//            );
+//
+//            echo 'created stock: ' . $row->id . '<br/>';
+//        }
 
-            $handle = fopen("/asxStock/asx.csv", "r");
 
-            while(! feof($handle))
-            {
-                $stock = fgetcsv($handle);
+        Excel::load('/../../askStock/asx.csv')->each(function (Collection $csvLine) {
 
-                $stock[3] = basename($handle, '.csv');
+            asxList::create([
+                'name' => $csvLine->get('name'),
+                'symbol' => $csvLine->get('symbol'),
+                'company' => $csvLine->get('company'),
+            ]);
 
-                if(count($stock) == 4)
-                {
-                    $list[] = $stock;
-                }
-            }
-
-        fclose($handle);
-
-        /**  ppd($stocks); **/
-
-        echo count($list) . ' to create<br/>';
-
-        foreach($list as $stock)
-        {
-            $row = asxList::create(
-                [
-                    'name' => $stock[0],
-                    'symbol' => $stock[1],
-                    'updated_at' => '5'
-                ]
-            );
-
-            echo 'created stock: ' . $row->id . '<br/>';
-        }
-
+        });
 
     }
 }
