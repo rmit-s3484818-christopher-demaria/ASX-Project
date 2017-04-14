56a82b0a2f04bda6f0495566d916b09be3987763
rmit-s3432729-andrew-sekulovski <s3432729@student.rmit.edu.au>
2017-04-14 21:16:06 +1000

    New sidebar
    Responsive for all device sizes now.

--- asx/resources/views/shared/navbar.blade.php
+++ asx/resources/views/shared/navbar.blade.php
@@ -1,42 +1,51 @@
 <!-- NAV BAR - Generic sticky navbar, improvements will be made later -->
-<div class="sidebarNav">
+
     <!-- Sidebar -->
     <div class="sidebarNav">
 
-        <div class="row-fluid">
-            <div class="row-fluid playerContainer">
-                <div class="col-lg-8 col-md-8 col-lg-offset-1 col-md-offset-1 playerTile"><h3><span class="glyphicon navbar-icons glyphicon-user"></span>user10112</h3></div> <!-- ensure username is max 15 characters long -->
-            </div>
-
+        {{--<div class="row-fluid">--}}
+            {{--<div class="row-fluid playerContainer">--}}
+                {{--<div class="col-lg-10 col-md-10 col-lg-offset-1 col-md-offset-1 playerTile"><span class="glyphicon navbar-icons glyphicon-user"></span><h5>user10112</h5></div> <!-- ensure username is max 15 characters long -->--}}
+            {{--</div>--}}
+        {{--</div>--}}
+        <ul class="playerContainer">
+            <li class="playerTile navItem"><span class="glyphicon navbar-icons glyphicon-user"></span><h4 class="sideBarText">Username</h4></li>
+        </ul>
             <ul class="sidebarOptions">
-                <a href="{{ url("/")}}"><li class="dashboardTile navItem"><h4><span class="glyphicon navbar-icons glyphicon-dashboard"></span>Dashboard</h4></li></a>
+
+                <a href="{{ url("/")}}"><li class="dashboardTile navItem"><span class="glyphicon navbar-icons glyphicon-dashboard"></span><h4 class="sideBarText">Dashboard</h4></li></a>
                 @if (Auth::check())
-                    <a href="{{ url("account") }}"> <li class="portfolioTile navItem"><h4><span class="glyphicon glyphicon-book"></span>Portfolio</h4></li></a>
+                    <a href="{{ url("account") }}"> <li class="portfolioTile navItem"><span class="glyphicon glyphicon-book"></span><h4 class="sideBarText">Portfolio</h4></li></a>
                 @endif
                 {{--<li class="portfolioTile"><h4>Portfolio</h4></li>--}}
-                <a href="{{ url("leader")}}"><li class="leaderboardTile navItem"><h4><span class="glyphicon navbar-icons glyphicon-king"></span>Leaderboard</h4></li></a>
-                <a href="{{ url("market")}}"><li class="marketTile navItem"><h4><span class="glyphicon navbar-icons glyphicon-stats"></span>Market</h4></li></a>
-                <a href="{{ url("asxList")}}" class="btn btn-info navItem" role="button">Link Button</a>
-                {{--<li class="leaderboardTile"><h4>Leaderboard</h4></li>--}}
-                {{--<li class="marketTile"><h4>Market</h4></li>--}}
-                @if (Route::has('login'))
+                <a href="{{ url("leader")}}"><li class="leaderboardTile navItem"><span class="glyphicon navbar-icons glyphicon-king"></span><h4 class="sideBarText">Rankings</h4></li></a>
+                <a href="{{ url("market")}}"><li class="marketTile navItem"><span class="glyphicon navbar-icons glyphicon-stats"></span><h4 class="sideBarText">Market</h4></li></a>
+                {{--<a href="{{ url("asxList")}}" class="btn btn-info navItem" role="button">Link Button</a>--}}
 
-                    @if (Auth::check())
-                        <a href="{{ url('auth/logout') }}"><li><h4>Logout</h4></li></a>
-                    @else
-                        <a href="{{ url('/login') }}"><li><h4>Login</h4></li></a>
-                        <a href="{{ url('/register') }}"><li><h4>Register</h4></li></a>
+                {{--@if (Route::has('login'))--}}
 
-                    @endif
+                    {{--@if (Auth::check())--}}
+                        {{--<a href="{{ url('auth/logout') }}"><li><h4 class="sideBarText">Logout</h4></li></a>--}}
+                    {{--@else--}}
+                        {{--<a href="{{ url('/login') }}"><li><h4 class="sideBarText">Login</h4></li></a>--}}
+                        {{--<a href="{{ url('/register') }}"><li><h4 class="sideBarText">Register</h4></li></a>--}}
 
-                @endif
+                    {{--@endif--}}
+
+                {{--@endif--}}
             </ul>
 
-            <div class="row-fluid sidebarBottom">
+            <div class="row-fluid sidebarBottom ">
+                <ul class="sidebarOptions">
+                    <a href="{{ url("settings")}}"><li class=" bottomNav navItem"><span class="glyphicon navbar-icons glyphicon-cog"></span><h4 class="sideBarText">Settings</h4></li></a>
+                    <br>
+                    <a href="{{ url("/")}}"><li class=" logoutNav navItem"><span class="glyphicon navbar-icons glyphicon-off"></span><h4 class="sideBarText">Logout</h4></li></a>
+                </ul>
                 {{--<div class="col-lg-1 col-md-1 col-md-offset-1 col-lg-offset-1 sidebarBottomContainer"><button class="btn btn-default" ><a href="{{ url("settings") }}">Settings</button></div>--}}
                 @if (Auth::check())
                 <div class="col-lg-1 col-lg-offset-5 col-md-offset-5 col-md-1 sidebarBottomContainer"><button class="btn-danger btn-logout">Log Out</button></div>
                 @endif
+
             </div>
         </div>
     </div>
