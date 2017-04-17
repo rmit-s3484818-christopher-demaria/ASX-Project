c564750f5a04c6a251521689d839a46c975a8bfe
rmit-s3484818-christopher-demaria <s3484818@student.rmit.edu.au>
2017-04-17 15:39:51 +1000

    getting closer.....
    almost there!?!!!

--- asx/app/Http/Controllers/marketController.php
+++ asx/app/Http/Controllers/marketController.php
@@ -9,8 +9,24 @@ class marketController extends Controller
 {
     public function view ()
     {
-        $stocks = DB::table('asxes')->get();
+        $stocks = DB::table('asxes')->pluck('symbol');
 
-        return view('test',['stocks'=> $stocks]);
+        $filename = 'asx-list';
+ //       Excel::create($filename,function($stocks){
+            foreach($stocks as $stock)
+            {
+//                $tries++;
+                echo $stock;
+
+//                $dataURL = 'http://finance.yahoo.com/d/quotes.csv?s='.$symbol.'.AX&f=nac1p1';
+//                $data = file_get_contents($dataURL);
+//                echo $data;
+            }
+
+ //       })->export('csv');
+
+
+
+//        return view('test',['stocks'=> $stocks]);
     }
 }
