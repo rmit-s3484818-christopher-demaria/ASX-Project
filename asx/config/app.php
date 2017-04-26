dac9809ab0e34e509bf584073c2370bd756d9755
rmit-s3484818-christopher-demaria <s3484818@student.rmit.edu.au>
2017-04-26 21:01:25 +1000

    We now have laravel datatables
    YOU NEED TO UPDATE COMPOSER

--- asx/config/app.php
+++ asx/config/app.php
@@ -178,6 +178,9 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Maatwebsite\Excel\ExcelServiceProvider::class,
+        Yajra\Datatables\DatatablesServiceProvider::class,
+
+
     ],
 
     /*
