c9da705136c5100039a0205363008db7a36dad7f
rmit-s3484818-christopher-demaria <s3484818@student.rmit.edu.au>
2017-05-23 21:33:01 +1000

    added admin page

---
+++ admin.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.master')
+@section('title')
+    Admin
+@stop
+@section('body')
+    <?php
+    $users = DB::table('users')->get();
+    ?>
+    <div class="navbarMargin">
+        <div class="container-fluid">
+            <div class="container-fluid">
+                <div>
+                    <h2 class="pageHeading">Admin</h2>
+                    <table>
+                        <tr>
+                            <td>Username</td>
+                            <td align="right">Ban</td>
+                        </tr>
+                    @foreach($users as $user)
+                            <tr>
+                                <td>
+                                   {{$user->name}}
+                                </td>
+                                <td><button type="button" class="btn btn-danger">Ban</button></td>
+                            </tr>
+                    @endforeach
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
